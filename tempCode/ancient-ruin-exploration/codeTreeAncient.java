a86aa025db2c693d6de33bb6a2dae76463d8b132
munjo <moonjo960@gmail.com>
2024-05-12 19:41:46 +0900

    임시 수정

--- tempCode/ancient-ruin-exploration/codeTreeAncient.java
+++ tempCode/ancient-ruin-exploration/codeTreeAncient.java
@@ -46,29 +46,77 @@ public class codeTreeAncient {
 
         for(int k = 0; k < K; k++){
             int maxScore = -1;
-
+            int[] maxValues = new int [3];
             for(int tempr = 1; tempr <= 3; tempr++){
                 for(int tempc = 1; tempc <= 3; tempc++){
                     int [][] copyArray = copyArrayFromRoot(map);
-
-
                     for(int turn = 0; turn < 3; turn ++){
                         copyArray = turn90Map(tempr, tempc, copyArray);
-                        int tempScore = calculScore(copyArray);
-
+                        int tempScore = calculScore(copyArray, false);
+                        if(maxScore < tempScore) {
+                            maxScore = tempScore;
+                            maxValues[0] = tempr;
+                            maxValues[1] = tempc;
+                            maxValues[2] = turn;
+                        }
                     }
-
-
                 }
             }
+            if(maxValues[0] == 0 && maxValues[1] == 0)break;
+            System.out.println(maxValues[0] + " : " + maxValues[1] + " : " + maxValues[2] + " : " + maxScore);
+            printMap();
+            for(int i = 0; i < maxValues[2] + 1; i++){
+                map = turn90Map(maxValues[0], maxValues[1], map);
+            }
+            System.out.println("2");
+            printMap();
+            int totalScore = 0;
+            int tempScore = -1;
+            while (tempScore != 0){
+                tempScore = calculScore(map, true);
+                if(tempScore != -1){
+                    totalScore += tempScore;
+                }
+//                printMap();
+                reFill(map);
+//                System.out.println("3");
+//                printMap();
+            }
+            sb.append(totalScore + " ");
+
+
+
 
         }
-
+        bw.write(sb.toString());
+        bw.flush();
 
 
     }
 
-    private static int calculScore(int[][] copyArray) {
+    private static void printMap() {
+        for(int r=0;r<5;r++){
+            for(int c=0;c<5;c++){
+                System.out.print(map[r][c] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    private static void reFill(int[][] map) {
+        for(int r = 4;r >=0; r--){
+            for(int c = 0; c<5;c++){
+                if(map[r][c] == 0){
+                    map[r][c] = numbers[numberIndex];
+                    numberIndex = (numberIndex+1)%numbers.length;
+                }
+            }
+        }
+
+    }
+
+    private static int calculScore(int[][] copyArray, boolean deleteFlag) {
         boolean [][] visited = new boolean[5][5];
         int score = 0;
         for(int r=0;r<5;r++){
@@ -79,24 +127,41 @@ public class codeTreeAncient {
                 Queue<int[]> Q = new LinkedList<>();
                 int locVal = map[r][c];
                 Q.add(new int [] {r,c});
-                visited[r][c] = true;
+                vis
+
+            ited[r][c] = true;
+                int count = 0;
                 while(!Q.isEmpty()){
                     int[] tempLoc = Q.poll();
+                    count++;
+                    if(deleteFlag){
+                        copyArray[tempLoc[0]][tempLoc[1]] = 0;
+                    }
                     for(int d=0;d<4;d++){
                         int nr = tempLoc[0] + dr[d];
                         int nc = tempLoc[1] + dc[d];
 
-                        if(check(nr,nc) && !visited[nr][nc] && map[nr][nc] == locVal){
+                        if(checkMap(nr,nc) && !visited[nr][nc] && map[nr][nc] == locVal){
                             visited[nr][nc] = true;
+                            Q.add(new int [] {nr,nc});
 
                         }
                     }
 
                 }
+                if(count >= 3){
+                    score+= count;
+                }
 
             }
         }
 
+        return score;
+    }
+
+    private static boolean checkMap(int nr, int nc) {
+        if(nr >= 0 && nr < 5 && nc >= 0 && nc < 5)return true;
+        return false;
 
     }
 
@@ -111,10 +176,10 @@ public class codeTreeAncient {
 
         for(int r=0;r<3;r++){
             for(int c=0;c<3;c++){
-                int tempMapR = tempr + r;
-                int tempMapC = tempc + c;
-                int replaceR = tempr + 2 - c;
-                int replaceC = tempc + r;
+                int tempMapR = tempr + r - 1;
+                int tempMapC = tempc + c - 1;
+                int replaceR = tempr + 1 - c;
+                int replaceC = tempc + r - 1;
                 tempMap[tempMapR][tempMapC] = copyArray[replaceR][replaceC];
             }
         }
