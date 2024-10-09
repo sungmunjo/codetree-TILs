a44d2936b7406b4f27cc7b210c249048888d7a8c
sungmunjo <80288379+sungmunjo@users.noreply.github.com>
2024-10-09 21:55:41 +0900

    Title: 격자에 점 그리기, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241009/격자에 점 그리기/draw-points-on-grid.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, M;
+	
+	static int [][] checkMap;
+	
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringBuffer sb = new StringBuffer();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		checkMap = new int [N][N];
+		
+		for(int m = 0; m < M; m++) {
+			st = new StringTokenizer(br.readLine());
+			
+			int r = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			checkMap[r-1][c-1] = m + 1;
+			
+		}
+		
+		for(int r=0;r<N;r++) {
+			for(int c=0;c<N;c++) {
+				sb.append(checkMap[r][c] + " ");
+			}
+			sb.append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		
+	}
+}
